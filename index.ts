1456249488763330d1dcd9e2aa3b56d816e249dc
chon26909 <59905927+chon26909@users.noreply.github.com>
2022-02-11 18:01:41 +0700

    update index app

--- index.ts
+++ index.ts
@@ -1,11 +1,24 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+
 import { AdminRoute, VandorRoute } from './routes';
+import { MONGO_URI } from './config';
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended:true }));
 
 app.use('/admin', AdminRoute);
 app.use('/vandor', VandorRoute);
 
-app.listen(5000, () => { 
+mongoose.connect(MONGO_URI)
+
+.then(() => console.log('connected database'))
+.catch((err) => console.log(err))
+
+app.listen(4000, () => { 
+    console.clear()
     console.log('App listening to the port 5000')
 }) 
