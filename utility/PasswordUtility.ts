c6744ad2d9de88055938aa8489b833287ed955c9
chon26909 <59905927+chon26909@users.noreply.github.com>
2022-02-13 23:14:41 +0700

    validate password and validate signarure

--- utility/PasswordUtility.ts
+++ utility/PasswordUtility.ts
@@ -1,4 +1,9 @@
+import { Request } from 'express';
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import { APP_SECRET } from '../config';
+import { VandorPayload } from '../dto';
+import { AuthPayload } from '../dto/Auth.dto';
 
 export const GenrateSalt = async () => {
     return await bcrypt.genSalt();
@@ -6,4 +11,29 @@ export const GenrateSalt = async () => {
 
 export const GeneratePassword = async (password: string, salt: string) => {
     return await bcrypt.hash(password, salt);
+}
+
+export const ValidatePassword = async (enterPassword: string, savedPassword: string, salt: string) => { 
+
+    return await GeneratePassword(enterPassword,salt) === savedPassword;
+}
+
+export const GenerateSignature = (payload: VandorPayload) => { 
+    return jwt.sign(payload,APP_SECRET, { expiresIn:'1d' });
+
+}
+
+export const ValidateSignature = (req: Request) => { 
+    const signature = req.get('Authorization');
+
+    if (signature) {
+        const token = signature.split(' ')[1];
+        const payload = jwt.verify(token, APP_SECRET) as AuthPayload;
+        
+        req.user = payload;
+
+        return true;
+   }
+    return false;
+
 }
